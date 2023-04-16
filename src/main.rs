68085d84909d31604614cebce256250d9cad01f1
jmillana <jordi@marimon.tech>
2023-04-16 13:25:28 +0200

    Start following the glium tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,35 @@
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::EventLoopBuilder;
-use winit::window::WindowBuilder;
-
-use glutin::config::ConfigTemplateBuilder;
-
-use glutin::prelude::*;
-
-use glutin_winit::{self, DisplayBuilder};
-
-pub mod gl {
-    #![allow(clippy::all)]
-    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
-
-    pub use Gles2 as Gl;
-}
+use glium::glutin::event as GlutinEvent;
+use glium::{glutin, Surface};
+use std::time;
 
 fn main() {
-    let event_loop = EventLoopBuilder::new().build();
-    let window_builder = if cfg!(wgl_backend) {
-        Some(WindowBuilder::new().with_transparent(false))
-    } else {
-        None
-    };
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let window_builder = glutin::window::WindowBuilder::new();
+    let context_builder = glutin::ContextBuilder::new();
+    let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
 
-    let template = ConfigTemplateBuilder::new();
-    let display_builder = DisplayBuilder::new().with_window_builder(window_builder);
+    event_loop.run(move |event, _, control_flow| {
+        let next_frame_time = time::Instant::now() + time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
-    let (_window, gl_config) = display_builder
-        .build(&event_loop, template, |configs| {
-            configs
-                .reduce(|accum, config| {
-                    let transparency_check = config.supports_transparency().unwrap_or(false)
-                        & !accum.supports_transparency().unwrap_or(false);
-                    if transparency_check || config.num_samples() > accum.num_samples() {
-                        config
-                    } else {
-                        accum
-                    }
-                })
-                .unwrap()
-        })
-        .unwrap();
-    println!("Picked a config with {} samples", gl_config.num_samples());
-
-    //let raw_window_handle = window.as_ref().map(|window| window.raw_window_handle());
-    /*let gl_display = gl_config.display();
-    // The context creation part. It can be created before surface and that's how
-    // it's expected in multithreaded + multiwindow operation mode, since you
-    // can send NotCurrentContext, but not Surface.
-    let context_attributes = ContextAttributesBuilder::new().build(raw_window_handle);
-    let fallback_context_attributes = ContextAttributesBuilder::new()
-        .with_context_api(ContextApi::Gles(None))
-        .build(raw_window_handle);
-
-    /*let _not_current_gl_context = Some(unsafe {
-        gl_display
-            .create_context(&gl_config, &context_attributes)
-            .unwrap_or_else(|_| {
-                gl_display
-                    .create_context(&gl_config, &fallback_context_attributes)
-                    .expect("failed to create context")
-            })
-    });*/*/
-
-    event_loop.run(move |event, _window_target, control_flow| {
-        control_flow.set_wait();
         match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => {
-                    control_flow.set_exit();
+            GlutinEvent::Event::WindowEvent { event, .. } => match event {
+                GlutinEvent::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
                 }
-                _ => (),
+                _ => return,
             },
-            _ => (),
+            GlutinEvent::Event::NewEvents(cause) => match cause {
+                GlutinEvent::StartCause::ResumeTimeReached { .. } => (),
+                GlutinEvent::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
         }
-    })
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+    });
 }
